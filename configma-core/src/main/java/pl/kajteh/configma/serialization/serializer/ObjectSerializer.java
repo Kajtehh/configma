ba04a01607654824cff86c9361e434c4b89b8650
Kajtehh <111462649+Kajtehh@users.noreply.github.com>
2025-08-24 16:09:46 +0200

    Update ObjectSerializer.java

--- configma-core/src/main/java/pl/kajteh/configma/serialization/serializer/ObjectSerializer.java
+++ configma-core/src/main/java/pl/kajteh/configma/serialization/serializer/ObjectSerializer.java
@@ -4,6 +4,6 @@ import pl.kajteh.configma.serialization.SerializationData;
 import pl.kajteh.configma.serialization.SerializedData;
 
 public interface ObjectSerializer<T> extends Serializer {
-    void serialize(final SerializationData data, T t);
+    void serialize(final SerializationData data, final T t);
     T deserialize(final SerializedData data);
 }
